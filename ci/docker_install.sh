2acd2a48263f413494ab014b2a16eb8adb6baead
afandiyusuf <afandi.yusuf.04@gmail.com>
2017-05-22 18:41:15 +0700

    update travis

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -3,18 +3,18 @@
 # We need to install dependencies only for Docker
 [[ ! -e /.dockerenv ]] && exit 0
 
-set -xe
+sudo set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
-apt-get update -yqq
-apt-get install -y -qq git curl wget php5-cli zip unzip
+sudo apt-get update -yqq
+sudo apt-get install -y -qq git curl wget php5-cli zip unzip
 
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # Install phpunit, the tool that we will use for testing
-curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
+sudo curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+sudo chmod +x /usr/local/bin/phpunit
 
 # Install mysql driver
 # Here you can install any other extension that you need
-docker-php-ext-install pdo_mysql
+sudo docker-php-ext-install pdo_mysql
